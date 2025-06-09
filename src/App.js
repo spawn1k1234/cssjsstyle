37bee601446df92a3bf1b7de5ce1153848989abf
spawn1k1234 <vladislav.sidorov2008@gmail.com>
2025-06-09 20:19:28 +0200

    Fix21

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./App.css";
+import RecipeCard from "./components/RecipeCard/RecipeCard";
+
+function App() {
+  const recipes = [
+    {
+      Image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCrb9lxOr0AUBl6muGyfwGjzryOqz7KesoNQ&s",
+      title: "Smoked salmon burger",
+      time: "20 min",
+      servings: "5 servings",
+      calories: "210 calories",
+      difficulty: ["Easy", "Medium", "Fixed"],
+    },
+    {
+      Image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbior8damh5OrkSj9qsqE0da5IucAx7n0Sqw&s",
+      title: "Tomatoes With Creamy Feta",
+      time: "15 min",
+      servings: "3 servings",
+      calories: "600 calories",
+      difficulty: ["Easy", "Medium", "Fixed"],
+    },
+    {
+      Image:
+        "https://mccormick.widen.net/content/uvjk9yurf1/original/spicy_potato_salad_637390546085236844_800x800.jpg",
+      title: "Spicy potato salad",
+      time: "30 min",
+      servings: "2 servings",
+      calories: "320 calories",
+      difficulty: ["Easy", "Medium", "Fixed"],
+    },
+    {
+      Image:
+        "https://www.cubesnjuliennes.com/wp-content/uploads/2020/07/Chicken-Biryani-Recipe.jpg",
+      title: "Chicken Biryani",
+      time: "40 min",
+      servings: "4 servings",
+      calories: "700 calories",
+      difficulty: ["Easy", "Medium", "Fixed"],
+    },
+  ];
+
+  return (
+    <div className="app">
+      <h1 className="app-title">Recipe Cards</h1>
+      <div className="recipes-container">
+        {recipes.map((recipe, index) => (
+          <RecipeCard key={index} recipe={recipe} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./RecipeCard.css";
+
+const RecipeCard = ({ recipe }) => {
+  return (
+    <div className="recipe-card">
+      {recipe.Image && (
+        <img src={recipe.Image} alt={recipe.title} className="recipe-image" />
+      )}
+
+      <h2 className="recipe-title">{recipe.title}</h2>
+      <div className="recipe-meta">
+        <span>⏱️ {recipe.time}</span>
+        <span>🍽️ {recipe.servings}</span>
+        <span>🔥 {recipe.calories}</span>
+      </div>
+      <div className="divasd">
+        <h3 className="difficulty-title">Difficulty</h3>
+        <ul className="difficulty-list">
+          {recipe.difficulty.map((level, index) => (
+            <li key={index} className="difficulty-item">
+              <strong>{level}</strong>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="divider"></div>
+    </div>
+  );
+};
+
+export default RecipeCard;
