b8b54f63cf76eba621e9ce57e049d4e553e7a197
spawn1k1234 <vladislav.sidorov2008@gmail.com>
2025-06-09 18:39:10 +0200

    Fix21

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const myName = "Владислав";
+
+  const imageUrl =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmd1rk8JVagwnBmpHYmx7qfcWu5RuUuuFkOA&s";
+  const imageDescription = "Зображення";
+
+  const favoriteSite = {
+    name: "youtube",
+    url: "https://www.youtube.com/",
+  };
+
+  const number1 = 5;
+  const number2 = 10;
+  const sum = number1 + number2;
+
+  const colors = ["Червоний", "Синій", "Зелений"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>{myName}</h1>
+      <p>Ласкаво просимо до нашого сайту!</p>
+      <img src={imageUrl} alt={imageDescription} />
+      <p>
+        Перейдіть на{" "}
+        <a href={favoriteSite.url} target="_blank" rel="noopener noreferrer">
+          {favoriteSite.name}
         </a>
-      </header>
+      </p>
+      <p>Сума чисел: {sum}</p>
+      <ul>
+        {colors.map((color, index) => (
+          <li key={index}>{color}</li>
+        ))}
+      </ul>
     </div>
   );
 }
